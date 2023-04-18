11ed883edf1ad43460214c1a8a17272596599428
peter parker <thisisjustpeter@gmail.com>
2023-04-19 02:47:52 +0900

    (Init) Initialize 0420-live-coding Project Setup: add .gitignore and notesList for local database.

---
+++ notesList.js
@@ -0,0 +1,14 @@
+const notesList = [
+  {id: 1, author: 'peter', title: 'note1', content: 'hello world1'},
+  {id: 2, author: 'peter', title: 'note2', content: 'hello world2'},
+  {id: 3, author: 'siri', title: 'note3', content: 'hello world3'},
+  {id: 4, author: 'yuna', title: 'note4', content: 'hello world4'},
+  {id: 5, author: 'yeony', title: 'note5', content: 'hello world5'},
+  {id: 6, author: 'heetae', title: 'note6', content: 'hello world6'},
+  {id: 7, author: 'steven', title: 'note7', content: 'hello world7'},
+  {id: 8, author: 'egoing', title: 'note8', content: 'hello world8'},
+  {id: 9, author: 'barry', title: 'note9', content: 'hello world9'},
+  {id: 10, author: 'clark', title: 'note10', content: 'hello world10'},
+];
+
+module.exports = notesList;
